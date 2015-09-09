19c1d53e7e98699f614f031f6da558462df9f543
mythern <myth@overflow.no>
2015-09-09 14:19:59 +0200

    Added sample graph files

--- module2/test.py
+++ module2/test.py
@@ -8,3 +8,14 @@ from astar import *
 from datastructures import *
 from gac import *
 
+
+if __name__ == '__main__':
+    """
+    main method
+    """
+
+    g = Graph()
+
+    node_set = g.read_graph_from_file('graphs/graph01.txt')
+
+    print(node_set)
