087a35172af21bc3349803bbd95b4a526f4c38d5
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-09-17 15:08:07 +0200

    Added makefunc

--- module2/common.py
+++ module2/common.py
@@ -40,3 +40,18 @@ def fetch_boards_from_dir(rootdir='boards'):
             boards.append(full_path)
 
     return boards
+
+
+def make_func(var_names, expression, environment=globals()):
+    """
+    This function lets the user input constraint descriptions, at runtime, and
+    converting them into working code chunks
+    :param var_names: The variables for the constraints
+    :param expression: The expression to evaluate
+    :param environment:
+    :return:
+    """
+    args = ""
+    for n in var_names:
+        args = args + "," + n
+    return eval("(lambda " + args[1:] + ": " + expression + ")", environment)
--- module2/gac.py
+++ module2/gac.py
@@ -2,3 +2,21 @@
 #
 # Created by 'myth' on 9/9/15
 
+
+class GAC(object):
+
+    def __init__(self, graph=None):
+        self.graph = graph
+        self.variables = []
+        self.domains = {}
+        self.constraints = {}
+
+    def initialize(self):
+        pass
+
+    def domain_filtering(self):
+        pass
+
+    def rerun(self):
+        pass
+
