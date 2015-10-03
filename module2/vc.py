1af070975e2d0ae53f0a4e2a789885e6118a15ee
Håkon Ødegård Løvdal <hakloev@gmail.com>
2015-10-03 21:17:11 +0200

    comments

--- module2/vc.py
+++ module2/vc.py
@@ -11,6 +11,11 @@ from common import *
 class VCProblem(AStarProblem):
 
     def __init__(self, nodes, edges):
+        """
+        Constructor for VCProblem
+        :param nodes: nodes in the VC-problem
+        :param edges: edges between the nodes in the VC-problem
+        """
 
         self.constraints = {}
         for from_node, to_node in edges:
@@ -33,14 +38,25 @@ class VCProblem(AStarProblem):
         self.goal_node = None
 
     def get_start_node(self):
+        """
+        Get the initital state (start node) for this specific problem
+        :return: The state
+        """
         return self.initial_state
 
     def get_goal_node(self):
+        """
+        Get the final state for this problem, if and only if it exists
+        :return: The state
+        """
         return self.goal_node
 
     def get_all_successor_nodes(self, astar_state):
         """
         Fetches all successor nodes from a given CSP state
+        In this spesific problem that means all states with a domain
+        length greater than 1 for a random node
+        :return: The generated successor nodes
         """
 
         csp_state = astar_state.state
@@ -75,6 +91,8 @@ class VCProblem(AStarProblem):
         to the goal. Devising a good, and admissible, heuristic, is more of a challenge,
         since it is very hard to estimate the extent of domain reduction incurred by any
         run of the Domain-filtering loop.
+        :param: The state to calculate the heuristic on
+        :return: The heuristic for the given state
         """
 
         h = sum((len(domains) - 1) for domains in astar_state.state.nodes.values())
@@ -89,4 +107,10 @@ class VCProblem(AStarProblem):
         return h
 
     def arc_cost(self, astar_state):
+        """
+        Returns the arc cost for a given state.
+        Always returning 1 in this problem
+        :param astar_state: The state to calculate on
+        :return: The calculated arc cost
+        """
         return 1
